4dc68cda9ae8ebd3beb88f2016c4537e57fa7560
ssoso27 <sohi8290@naver.com>
2019-05-02 23:07:08 +0900

    [190502] spring-database 연결

--- Spring(boot) study/my-first-project/src/main/java/com/example/myfirstproject/model/Student.java
+++ Spring(boot) study/my-first-project/src/main/java/com/example/myfirstproject/model/Student.java
@@ -2,10 +2,7 @@ package com.example.myfirstproject.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data // getter, setter 등 자동생성
 @Entity(name = "Student") // Student 라는 도메인 생성
@@ -14,8 +11,15 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "hackbun")
     private String hackbun;
+
+    @Column(name = "birthday")
     private String birthday;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "gender")
     private String gender;
 }
